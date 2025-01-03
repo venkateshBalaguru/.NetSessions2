8392cb0cd79a3666a75f8ff2ddf11fbf1c8cb61e
venkatesh <venkatesh.balagurunathan@rrd.com>
2025-01-03 11:50:30 +0530

    This Day17 app

---
+++ Day17/HelloWorld/HelloWorld/Hello.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Hello
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Enter the Number to print How many times you want");
+        int No = Convert.ToInt32(Console.ReadLine());
+            
+        for (int i = 0; i < No; i++)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        }
+    }
+
