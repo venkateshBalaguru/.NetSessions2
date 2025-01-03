c51f11a2956625768c00204488a0255593b49b7f
venkatesh <venkatesh.balagurunathan@rrd.com>
2025-01-03 11:43:44 +0530

    my first project

---
+++ Day18/Day18HelloUsers/Day18HelloUsers/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Day18HelloUsers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!" );
+        }
+    }
+}
