f6eae4056649abcc92660ac930f94f651a295e1b
venkatesh <venkatesh.balagurunathan@rrd.com>
2025-01-05 16:21:55 +0530

    My first program

--- Day17/HelloWorld/HelloWorld/Hello.cs
+++ Day17/HelloWorld/HelloWorld/Hello.cs
@@ -1,15 +1,7 @@
-﻿using System;
-class Hello
+﻿class Hello
+{
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-        Console.WriteLine("Enter the Number to print How many times you want");
-        int No = Convert.ToInt32(Console.ReadLine());
-            
-        for (int i = 0; i < No; i++)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-        }
+        System.Console.WriteLine("Hello, World!");
     }
-
+}
