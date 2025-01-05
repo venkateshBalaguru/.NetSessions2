3dc20c68423cea3ca331c9764096d62278139a90
venkatesh <venkatesh.balagurunathan@rrd.com>
2025-01-05 16:29:27 +0530

    lab program

---
+++ Day17/HelloWorld2/HelloWorld2/Program.cs
@@ -0,0 +1,10 @@
+﻿class HelloWorld2
+{
+    static void Main()
+    {
+        Console.Write("Enter the Number How many times you want print: ");
+        var number = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < number; i++)    
+            Console.WriteLine("Hello, World!");
+    }
+}
