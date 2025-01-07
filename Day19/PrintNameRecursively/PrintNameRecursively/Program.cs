78a7327193bf5dad8fba0ea43f7e31306392e02b
venkatesh <venkatesh.balagurunathan@rrd.com>
2025-01-07 10:05:21 +0530

    changes

--- Day19/PrintNameRecursively/PrintNameRecursively/Program.cs
+++ Day19/PrintNameRecursively/PrintNameRecursively/Program.cs
@@ -3,16 +3,16 @@ class PrintNameRecursively
 {
     static void Main() //main Function
     {
-        PrintName(1);
+        PrintName(1); //Function call with argument
     }
-    static void PrintName(int number) //Recursive print function
+    static void PrintName(int number) //Print function
     {   
-        if(number <= 10) //condition to print the name 10 times
+        if(number <= 10) //condition to break the function
         {
             
             Console.WriteLine($"Hi! I am Venkatesh | {number}");
             number++;
-            PrintName(number);
+            PrintName(number);// (function called itself is called Recursive)
         }
         
        
