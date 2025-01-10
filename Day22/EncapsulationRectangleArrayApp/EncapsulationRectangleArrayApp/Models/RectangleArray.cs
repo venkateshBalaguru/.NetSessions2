3617d0f38386cae31a7a9849e964f9e2b8e8ba1a
venkatesh <venkatesh.balagurunathan@rrd.com>
2025-01-10 09:42:41 +0530

    Updated lab

---
+++ Day22/EncapsulationRectangleArrayApp/EncapsulationRectangleArrayApp/Models/RectangleArray.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationRectangleArrayApp.Models
+{
+     class RectangleArray
+    {
+        private int _height;
+        private int _width;
+        private string _color;
+        private const int MAX_DIMENSION= 100;
+        private const int MIN_DIMENSION = 1;
+
+        public int CalculatedAread()
+        {
+            return _height * _width;
+        }
+
+        public void SetHeight(int pheight)
+        {
+            if (pheight < 0)
+            {
+                pheight = MIN_DIMENSION;
+                _height = pheight;
+            }
+            
+
+            if (pheight > 100)
+            {
+                pheight = MAX_DIMENSION;
+                _height = pheight;
+            }
+            _height = pheight;
+        }
+
+        public int GetHeight()
+        {
+            return _height;
+        }
+
+        public void SetWidth(int pwidth)
+        {
+          _width = CheckDimension(pwidth);
+        }
+
+        public int GetWidth()
+        {
+            return _width;
+        }
+
+        public string SetColor(string pcolor)
+        {
+            if (pcolor == "red" || pcolor == "green" || pcolor == "blue")
+            {
+               return _color = pcolor;
+            }
+           return _color = "red"; //default color
+         }
+
+        public string GetColor()
+        {
+            return _color;
+        }
+
+        public int CheckDimension(int dimension)
+        {
+            if (dimension < 0)
+            {
+                return MIN_DIMENSION;
+            }
+            if (dimension > 100)
+            {
+                return MAX_DIMENSION;
+            }
+            return dimension;
+        }
+
+    }
+}
---
+++ Day22/EncapsulationRectangleArrayApp/EncapsulationRectangleArrayApp/RectangleArrayTest.cs
@@ -0,0 +1,45 @@
+﻿using EncapsulationRectangleArrayApp.Models;
+using System;
+class RectangleArrayTest
+{
+        static void Main(string[] args)
+        {
+            RectangleArray[] rectangles = new RectangleArray[3];
+            rectangles[0] = new RectangleArray();
+            rectangles[0].SetHeight(10);
+            rectangles[0].SetWidth(20);
+            rectangles[0].SetColor("red");
+        Console.WriteLine("Rectangle 1: Height = {0}, Width = {1}, Area = {2}, Color = {3}", rectangles[0].GetHeight(), rectangles[0].GetWidth(), rectangles[0].CalculatedAread(), rectangles[0].GetColor());
+
+        rectangles[1] = new RectangleArray();
+        rectangles[1].SetHeight(30);
+        rectangles[1].SetWidth(40);
+        rectangles[1].SetColor("green");
+        Console.WriteLine("Rectangle 2: Height = {0}, Width = {1}, Area = {2}, Color = {3}", rectangles[1].GetHeight(), rectangles[1].GetWidth(), rectangles[1].CalculatedAread(), rectangles[1].GetColor());
+
+
+        rectangles[2] = new RectangleArray();
+        rectangles[2].SetHeight(50);
+        rectangles[2].SetWidth(60);
+        rectangles[2].SetColor("yellow");
+        Console.WriteLine("Rectangle 3: Height = {0}, Width = {1}, Area = {2}, color = {3}", rectangles[2].GetHeight(), rectangles[2].GetWidth(), rectangles[2].CalculatedAread(), rectangles[2].GetColor());
+
+        BiggestRectangle(rectangles);
+    }
+
+    static void BiggestRectangle(RectangleArray[] rectangles)
+    {
+        int maxArea = 0;
+        int rectangleIndex = 0;
+        for (int i = 0; i < rectangles.Length; i++)
+        {
+            if (rectangles[i].CalculatedAread() > maxArea)
+            {
+                maxArea = rectangles[i].CalculatedAread();
+                rectangleIndex = i;
+            }
+        }
+       Console.WriteLine("The biggest rectangle is Rectangle {0} with area = {1}", rectangleIndex + 1, maxArea);
+    }
+}
+
