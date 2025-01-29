9291cf8c51579a3bd15def588489c0a518e2a93e
venkatesh <venkatesh.balagurunathan@rrd.com>
2025-01-29 09:30:25 +0530

    Day32

--- Day32/ReverseStringInSameArray/ReverseStringInSameArray/Program.cs
+++ Day32/ReverseStringInSameArray/ReverseStringInSameArray/Program.cs
@@ -1,16 +1,32 @@
-﻿namespace ReverseStringInSameArray
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ReverseStringInSameArray
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             string[] inputArray = { "Venkat", "Akhilesh", "Sachin" };
-
-          for (int i = inputArray.Length-1; i >= 0; i--)
+            string temp;
+            int arrayLength = inputArray.Length-1;
+            Console.WriteLine("Array in straight order\n======================");
+            PrintArray(inputArray);
+            for (int i = 0; i < inputArray.Length-1; i++)
             {
-                Console.WriteLine(inputArray[i]);
+                temp = inputArray[i];
+                inputArray[i] = inputArray[arrayLength];
+                inputArray[arrayLength] = temp;
+                arrayLength--;
             }
-
+            Console.WriteLine("\nArray in Reverse Order\n========================");
+            PrintArray(inputArray);
+        }
+        public static void PrintArray(string[] inputArray)
+        {
+            foreach (string input in inputArray)
+            {
+                Console.WriteLine(input);
+            }
         }
     }
 }
