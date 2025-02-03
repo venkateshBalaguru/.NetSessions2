e9abec9bf07acbbcf854bfb7e3cf921193f829d0
venkatesh <venkatesh.balagurunathan@rrd.com>
2025-02-03 10:16:02 +0530

    Day35

--- Day35/MyAttributeApp/MyAttributeApp/Program.cs
+++ Day35/MyAttributeApp/MyAttributeApp/Program.cs
@@ -8,7 +8,23 @@ namespace MyAttributeApp
         static void Main(string[] args)
         {
 
-           
+            List<MethodInfo> methodAttribute = new List<MethodInfo>();
+            MethodInfo[] methods = typeof(MyAttribute).GetMethods();
+
+
+            foreach (MethodInfo method in methods)
+            {
+                object[] attributes = method.GetCustomAttributes(typeof(ThisMethodNeedsAttentionAttribute), false);
+
+                if (attributes.Length > 0)
+                {
+                    methodAttribute.Add(method);
+                }
+            }
+            foreach (MethodInfo method in methodAttribute)
+            {
+                Console.WriteLine($"This \"{method.Name}\" needs Priority.");
+            }
         }
     }
 }
