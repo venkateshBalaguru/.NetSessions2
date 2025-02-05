44fea69b206b0ed83ac7f8ca404ee0f5f017b787
venkatesh <venkatesh.balagurunathan@rrd.com>
2025-02-05 23:39:08 +0530

    Day38

---
+++ Day38/TodoApp/TodoCommonLib/Models/TodoManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoCommonLib.Models
+{
+    public class TodoManager
+    {
+        public static void AddTodoList(string desc, string prioritize)
+        {
+            var details = $"{desc},{prioritize}";
+            FileStream fs = new FileStream(@"C:\venkat-html-app\DotNetSessions\Day37\Todo.txt", FileMode.Append);
+            //inserting the Data in to file
+            StreamWriter sw = new StreamWriter(fs);  //this is old type
+            sw.WriteLine(details);
+            sw.Close();
+            fs.Close();
+        }
+        public static void RemoveTodo()
+        {
+            Console.WriteLine("Enter the Description to Remove from Todo List");
+            var remove = Console.ReadLine();
+            string filePath = "C:\\venkat-html-app\\DotNetSessions\\Day37\\Todo.txt";
+            List<string> lstItem = File.ReadAllLines(filePath).Where(arg => !string.IsNullOrWhiteSpace(arg)).ToList();
+            lstItem.RemoveAll(x => x.Split(',')[0].Equals(remove));
+            File.WriteAllLines(filePath, lstItem);
+        }
+        public static void DisplayTodoList()
+        {
+            string filePath = "C:\\venkat-html-app\\DotNetSessions\\Day37\\Todo.txt";
+            List<Todo> todolist = new List<Todo>();
+
+            //Reading the file 
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] values = line.Split(',');
+                    for (int i = 0; i < 1; i++)
+                    {
+                        todolist.Add(new Todo(values[i], values[i + 1]));
+                    }
+                }
+                sr.Close();
+            }
+            Console.WriteLine("\nDetails of the TodoList\n==========================");
+            foreach (var value in todolist)
+            {
+                Console.WriteLine(value);
+            }
+
+
+        }
+    }
+}
