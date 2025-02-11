0ab86ad333fec379457c47f86f0d6072ba15042d
venkatesh <venkatesh.balagurunathan@rrd.com>
2025-02-12 00:34:07 +0530

    Day42

---
+++ Day42/TodoAppV3/TodoAppPresentation/Program.cs
@@ -0,0 +1,72 @@
+﻿using TodoCommonLib.Models;
+namespace TodoAppPresentation
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Todo App\n========");
+            Console.WriteLine("1. Add Todo");
+            Console.WriteLine("2. Display Todo");
+            Console.WriteLine("3. Update Todo's By ID");
+            Console.WriteLine("4. Remove Todo's by ID");
+            Console.WriteLine("5. Exit");
+            Console.Write("\nEnter the Number to Proceed : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if (number == 1)
+            {
+                EnterDetails();
+            }
+            else if (number == 2)
+            {
+                TodoManager.DisplayTodoList();
+                ToContinue();
+            }
+            else if (number == 3)
+            {
+                Console.Write("Enter the Id number to Update from the List : ");
+                var updateNumber = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter the Priority to Update");
+                var updatePriority = Console.ReadLine();
+                TodoManager.UpdateTodo(updateNumber, updatePriority);
+                ToContinue();
+            }
+            else if (number == 4)
+            {
+                Console.Write("Enter the Id number to remove from the List : ");
+                var removeNumber = Convert.ToInt32(Console.ReadLine());
+                TodoManager.RemoveTodo(removeNumber);
+                ToContinue();
+            }
+            else if (number == 5)
+            {
+                Console.WriteLine("Exit the App");
+            }
+            else
+            {
+                Console.WriteLine("Enter the Correct Option");
+                ToContinue();
+            }
+        }
+
+        public static void EnterDetails()
+        {
+            Console.WriteLine("Enter the Description of the Todo :");
+            var desc = Console.ReadLine();
+            Console.WriteLine("Enter the Priority");
+            var prioritize = Console.ReadLine();
+            TodoManager.AddTodoList(desc, prioritize);
+            ToContinue();
+        }
+        public static void ToContinue()
+        {
+            Console.WriteLine("Do You want to Continue Press \"Y\"");
+            var key = Console.ReadLine();
+            if (key == "y" || key == "Y")
+                Main();
+            else
+                Console.WriteLine("Exit the App");
+        }
+    }
+}
---
+++ Day42/TodoAppV3/TodoCommonLib/Models/TodoManager.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlTypes;
+using System.Diagnostics;
+using Microsoft.Data.SqlClient;
+using System.Data.SqlClient;
+
+namespace TodoCommonLib.Models
+{
+    public class TodoManager
+    {
+        public static void AddTodoList(string desc, string prioritize)
+        {
+            var conString = "server=.\\sqlexpress;database=rrd_db1;Integrated security=true; TrustServerCErtificate=true";
+            var con = new SqlConnection(conString);
+            var cmd = new SqlCommand($"Insert into TodoList (Description,Priority) values ('{desc}','{prioritize}')", con);
+
+            try
+            {
+                con.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                Console.WriteLine($"{rowsAffected} row(s) inserted.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        } 
+
+        public static void RemoveTodo(int removeNumber)
+        {
+            var conString = "server=.\\sqlexpress;database=rrd_db1;Integrated security=true; TrustServerCErtificate=true";
+            var con = new SqlConnection(conString);
+            var cmd = new SqlCommand($"delete TodoList where ID = {removeNumber}", con);
+            try
+            {
+                con.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                Console.WriteLine($"{rowsAffected} row(s) Deleted.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+        public static void UpdateTodo(int updateNumber, string updatePriority)
+        {
+            var conString = "server=.\\sqlexpress;database=rrd_db1;Integrated security=true; TrustServerCErtificate=true";
+            var con = new SqlConnection(conString);
+            var cmd = new SqlCommand($"Update TodoList set Priority = '{updatePriority}' where ID = {updateNumber}", con);
+            try
+            {
+                con.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                Console.WriteLine($"{rowsAffected} row(s) Updated.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+        public static void DisplayTodoList()
+        {
+            var connectionString = "server=.\\sqlexpress;database=rrd_db1;Integrated security=true;TrustServerCertificate=true";
+            var con = new SqlConnection(connectionString);
+            var cmd = new SqlCommand("select * from" +
+                " TodoList", con);
+            con.Open();
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.Write(reader["ID"] + " --> ");
+                Console.Write(reader["Description"] + " || ");
+                Console.Write(reader["Priority"] + " || ");
+                Console.WriteLine();
+
+            }
+            con.Close();
+
+        }
+    }
+}
