4b1b76a9df405e55f09de2e80aa9479e1f391b1d
venkatesh <venkatesh.balagurunathan@rrd.com>
2025-02-18 09:55:30 +0530

    Day46

---
+++ Day46/EmployeeStreamReader/EmployeeStreamReader/Models/Employee.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeStreamReader.Models
+{
+    internal class Employee
+    {
+        private int _id;
+        private string _name;
+        private string _desigination;
+        private string _mgrId;
+        private string _doj;
+        private string _sal;
+        private string _com;
+        private string _deptNo;
+
+               
+
+        public Employee(int id, string name, string desigination,string mgrId, string doj, string salary, string commission, string deptno) 
+        {
+            _id = id;
+            _name = name;
+            _desigination = desigination;
+            _mgrId = mgrId;
+            _doj = doj;
+            _sal = salary;
+            _com = commission;
+            _deptNo = deptno;
+
+
+        }
+        public int Id{get { return _id; }}
+        public string Name { get { return _name; } }
+        public string Desigination {  get { return _desigination; } }
+
+        public string MgrID { get { return _mgrId; }}
+        public string Doj { get { return _doj; }}
+        public string Salary { get { return _sal; }}
+        public string Commission { get { return _com; }}
+        public string DeptNo { get { return _deptNo; }}
+
+
+
+        public override int GetHashCode()
+        {
+            return Id;
+        }
+
+        public override bool Equals(object other)
+    {
+        Employee second = (Employee)other;
+        if (this.Id == second.Id )
+        {
+            return true;
+        }
+        return false;
+    }
+    //public override string ToString()
+    //{
+    //    return $"id is  {this.Id} , name is {this.Name} , desigination is {this.Desigination}";
+    //}
+}
+}
---
+++ Day46/EmployeeStreamReader/EmployeeStreamReader/Program.cs
@@ -0,0 +1,114 @@
+﻿using System.Globalization;
+using System.Linq;
+using EmployeeStreamReader.Models;
+
+namespace EmployeeStreamReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = "C:\\venkat-html-app\\DotNetSessions\\Day46\\EmployeeStreamReader\\Employee.csv";
+            char charToTrim = '\'';
+            HashSet<Employee> empData = new HashSet<Employee>();  //using HashSet for getting the unique values
+
+
+            //Reading the file 
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line;
+                bool isFirstLine = true;  //For Ignoring the first line
+                //Iterating until null
+                while ((line = sr.ReadLine()) != null)
+                {
+                    //First line caption was removing and inserting the remaining lines
+                    if (isFirstLine)
+                    {
+                        isFirstLine = false;
+                        continue;
+                    }
+                    string[] values = line.Split(',');
+                    for (int i = 0; i < 1; i++)
+                    {
+                        var empId = Convert.ToInt32(values[i]); 
+                        empData.Add(new Employee(empId, values[i + 1], values[i + 2], values[i+3], values[i + 4], values[i + 5], values[i + 6], values[i+7]));
+                    }
+                }
+                sr.Close();
+            }
+
+            //Printing the employees Details from the file with only Unique datas
+            Console.WriteLine("Details of the Employees\n==========================");
+            foreach (var value in empData)
+            {
+                Console.WriteLine($"EmpId : {value.Id} | EmpName : {value.Name.Trim(charToTrim)} | Desigination : {value.Desigination.Trim(charToTrim)} | MGRId : {value.MgrID.Trim(charToTrim)} | DOJ : {value.Doj.Trim(charToTrim)} | EmpSalary : {value.Salary.Trim(charToTrim)} | Commission : {value.Commission.Trim(charToTrim)} | DeptNo : {value.DeptNo.Trim(charToTrim)}");
+            }
+            FindEmpName(empData);
+            EmpMgrId(empData);
+            DeptNo(empData);
+            //getting the method value in to variable
+            //var employee = empGroupByDesg(empData);
+            //PrintDetails(employee);
+        }
+        //Grouping the Emp Data with the Desigination
+        //public static Dictionary<string, List<Employee>> empGroupByDesg(HashSet<Employee> empData)
+        //{
+        //    var map = new Dictionary<string, List<Employee>>();
+        //    foreach (Employee emp in empData)
+        //    {
+        //        if (!map.ContainsKey(emp.Desigination))
+        //            //groupByLocation[customer.Location] = new List<Customer>();//we can do like this also 
+        //            map.Add(emp.Desigination, new List<Employee>());
+        //        map[emp.Desigination].Add(emp);
+        //    }
+        //    return map;
+        //}
+
+        //public static void PrintDetails(Dictionary<string, List<Employee>> employees)
+        //{
+        //    char charToTrim = '\''; //Triming the unwanted characters
+        //    Console.WriteLine($"\nGrouping By Desigination:\n===========================");
+        //    foreach (var employee in employees)
+        //    {
+        //        Console.WriteLine($"\n{employee.Key.Trim(charToTrim)}\n-------------------");
+        //        foreach (var emp in employee.Value)
+        //        {
+        //            Console.WriteLine($"ID : {emp.Id} || Name : {emp.Name.Trim(charToTrim)}");
+        //        }
+        //    }
+        //}
+        private static void FindEmpName(HashSet<Employee> empData)
+        {
+            var employee = empData.Single(x => x.Name == "'KING'");
+            Console.WriteLine("select employee Name is King");
+            Console.WriteLine($"{employee.Id} | {employee.Name} | {employee.MgrID} | {employee.Desigination} ");
+        }
+
+        private static void DeptNo(HashSet<Employee> empData)
+        {
+            var DeptList = empData.Where(e => e.DeptNo.Contains("10")).AsEnumerable();
+            var enumerator = DeptList.GetEnumerator();
+            Console.WriteLine("Select Dept where dept Id=10");
+            while (enumerator.MoveNext())
+            {
+                var employee = enumerator.Current;
+                Console.WriteLine($"{employee.Id} | {employee.Name} | {employee.MgrID} | {employee.Desigination} |{employee.DeptNo}");
+            }
+        }
+
+        private static void EmpMgrId(HashSet<Employee> empData)
+        {
+            Console.WriteLine("Select Manager from list");
+            var ManagerIdList = empData.Where(e => e.Desigination.Contains("MANAGER")).AsEnumerable();
+            var enumerator = ManagerIdList.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                var employee = enumerator.Current;
+                Console.WriteLine($"{employee.Id} | {employee.Name} | {employee.MgrID} | {employee.Desigination} ");
+            }
+
+        }
+    }
+}
+
+   
